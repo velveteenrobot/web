6f1f43dae8b7424699b3a3c763fdf75a81f16d00
Sarah Elliott <sksellio@gmail.com>
2012-04-05 17:06:21 -0700

    Logging in actually works now. Still assumes rosbridge is running.

--- web_abominations/login.js
+++ web_abominations/login.js
@@ -1,39 +1,64 @@
 <!-- Begin
 
 function login(form) {
-  if (form.username.value != null && form.password.value != null) {
-     connection.callService('/login','[\"' + form.username.value + "\", \"" + form.password.value + '\"]',function(resp) {
-      if (resp != null && resp.token != 0) {
-        //go to the web abomination but also tell it stuff
-        //Could put the token in the url, but not admin thing, super secret HTTP fun?
-        var token = resp.token;
-        var is_admin = resp.is_admin;
-        var url_array = window.location.href.split('/');
-        var url = 'http://' + url_array[2] + "web_abomination.html";
-        self.location.href = url;
-      } else {
-        alert("Bad try at logging in.");
-      }
-    });
-  } else {
-    alert("Don't leave stuff blank.");
-    //form.username.focus();
-  }
+  var url_array = window.location.href.split('/');
+  var connection = new ros.Connection("ws://" + url_array[2] + ":9091");
+  connection.setOnClose(function (e) {
+    alert('connection closed<br/>');
+    //start up rosbridge, reload?
+  });
+  connection.setOnError(function (e) {
+    alert('error!<br/>');
+  });
+  connection.setOnOpen(function (e) {
+    alert('connected to ROS<br/>');
+    if (form.username.value != null && form.password.value != null) {
+       connection.callService('/login','[\"' + form.username.value + "\", \"" + form.password.value + '\"]',function(resp) {
+        if (resp != null && resp.token != 0) {
+          //go to the web abomination but also tell it stuff
+          //Could put the token in the url, but not admin thing, super secret HTTP fun?
+          var token = resp.token;
+          var is_admin = resp.is_admin;
+          var url_array = window.location.href.split('/');
+          var url = 'http://' + url_array[2] + "web_abomination.html";
+          self.location.href = url;
+        } else {
+          alert("Bad try at logging in.");
+        }
+      });
+    } else {
+      alert("Don't leave stuff blank.");
+      //form.username.focus();
+    }   
+  //only make the objects visible if rosbridge connected?
+  });
+ 
 }
 
 function newUser(form) {
-  if (form.username.value != null && form.password.value != null) {
-    connection.callService('/create_user','[\"' + form.username.value + "\", \"" + form.password.value + '\"]',function(resp) {
-      if (resp != null && resp.token != 0) {
-        login(form);
-      } else {
-        alert("Bad try at logging in.");
-      }
-    });
-  } else {
-    alert("Don't leave stuff blank.");
-    //form.username.focus();
-  }  
+  var url_array = window.location.href.split('/');
+  var connection = new ros.Connection("ws://" + url_array[2] + ":9091");
+  connection.setOnClose(function (e) {
+    alert('connection closed<br/>');
+    //start up rosbridge, reload?
+  });
+  connection.setOnError(function (e) {
+    alert('error!<br/>');
+  });
+  connection.setOnOpen(function (e) {
+    if (form.username.value != null && form.password.value != null) {
+      connection.callService('/create_user','[\"' + form.username.value + "\", \"" + form.password.value + '\"]',function(resp) {
+        if (resp != null && resp.token != 0) {
+          login(form);
+        } else {
+          alert("Bad try at logging in.");
+        }
+      });
+    } else {
+      alert("Don't leave stuff blank.");
+      //form.username.focus();
+    }
+  });  
 }
 
 
